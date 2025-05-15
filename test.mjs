0805383e6e145c60972ced231b1206c02daf060a
Dan Jackson <email@danieljackson.co.uk>
2025-05-15 16:30:26 +0100

    -

--- test.mjs
+++ test.mjs
@@ -23,8 +23,12 @@ outputImageTerminal(colorData, result.width, result.height);
 console.log();
 outputImageTerminalSmall(colorData, result.width, result.height);
 console.log();
+if (1) {
+  const sixelData = generateImageTerminalSixel(colorData, result.width, result.height, 2);
+  console.log(sixelData);
+}
 
-function outputImageTerminal(color, width, height) {
+function outputImageTerminal(colorData, width, height) {
   const t = '██'; // '██'; // '██', '▓▓', '▒▒', '░░'
   for (let y = 0; y < height; y++) {
     const lineParts = [];
@@ -37,7 +41,7 @@ function outputImageTerminal(color, width, height) {
   }
 }
 
-function outputImageTerminalSmall(color, width, height) {
+function outputImageTerminalSmall(colorData, width, height) {
   for (let y = 0; y < height; y += 2) {
     const lineParts = [];
     for (let x = 0; x < width; x++) {
@@ -58,4 +62,83 @@ function outputImageTerminalSmall(color, width, height) {
     lineParts.push('\x1B[0m');
     console.log(lineParts.join(''));
   }
+}
+
+
+function generateImageTerminalSixel(colorData, width, height, scale) {
+    const LINE_HEIGHT = 6;
+    const parts = [];
+
+    // Calculate mapping to sixel color code
+    const toCode = {};
+    for (let y = 0; y < height; y ++) {
+        for (let x = 0; x < width * scale; x += scale) {
+            const c = colorData[y * width + x]; // c[0], c[1], c[2]
+            if (c == null) continue;
+            const r = Math.floor(100 * c[0] / 255);
+            const g = Math.floor(100 * c[1] / 255);
+            const b = Math.floor(100 * c[2] / 255);
+            const code = `${r};${g};${b}`;
+            toCode[c] = code;
+        }
+    }
+    // Calculate unique color codes
+    const codeMap = {};
+    for (const c of Object.values(toCode)) {
+      codeMap[c] = true;
+    }
+    const codes = Object.keys(codeMap);
+
+    // Enter sixel mode
+    parts.push('\x1BP7;1q');    // 1:1 ratio, 0 pixels remain at current color
+    // Set color map
+    parts.push('#0;2;0;0;0');       // Background
+    for (let y = 0; y < height * scale; y += LINE_HEIGHT) {
+        let passCount = 0;
+        for (let pass of codes) {
+            // Start a pass in a specific color
+            const passStart = (passCount++ == 0 ? '' : '$') + '#' + 1 + ';2;' + pass;
+            let lastX = 0;
+            // Line data
+            for (let x = 0; x < width * scale; x += scale) {
+                let value = 0;
+                for (let yy = 0; yy < LINE_HEIGHT; yy++) {
+                    const c = colorData[Math.floor((y + yy) / scale) * width + Math.floor(x / scale)];
+                    if (c == null) continue;
+                    const code = toCode[c]; // c[0], c[1], c[2]
+                    if (code == null) continue;
+                    if (code != pass) {
+                        // Not the current color
+                        continue;
+                    }
+                    value |= 1 << yy;
+                }
+                if (value > 0) {
+                  if (lastX == 0) {
+                    parts.push(passStart);
+                  }
+                  const gap = x - lastX;
+                  if (gap > 0) {
+                    // Gap of empty pixels to the current position
+                    if (gap <= 3) {
+                      parts.push('?'.repeat(gap));
+                    } else {
+                      parts.push('!' + gap + '?');
+                    }
+                  }
+                  const code = (scale > 3 ? '!' + scale : '') + String.fromCharCode(value + 63).repeat(scale <= 3 ? scale : 1);
+                  // Six pixels strip at 'scale' (repeated) width
+                  parts.push(code);
+                  lastX = x + scale;
+                }
+            }
+        }
+        // Next line
+        if (y + LINE_HEIGHT < height * scale) {
+            parts.push('-');
+        }
+    }
+    // Exit sixel mode
+    parts.push('\x1B\\');
+    return parts.join('');
 }
